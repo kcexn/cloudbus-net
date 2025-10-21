c5e28443632456f877c68a01e2750ff334df6832
Kevin Exton <kevin.exton@pm.me>
2025-10-21 13:30:45 +1100

    chore: update test coverage.

--- tests/test_async_tcp_service.cpp
+++ tests/test_async_tcp_service.cpp
@@ -26,246 +26,6 @@
 #include <arpa/inet.h>
 using namespace cloudbus::service;
 
-struct async_noop_service : public async_tcp_service<async_noop_service> {
-  using Base = async_tcp_service<async_noop_service>;
-
-  template <typename T>
-  explicit async_noop_service(socket_address<T> address) : Base(address)
-  {}
-
-  auto operator()(async_context &ctx, const socket_dialog &socket,
-                  std::shared_ptr<read_context> rmsg,
-                  std::span<const std::byte> buf) -> void
-  {
-    msg.insert(msg.end(), buf.begin(), buf.end());
-    reader(ctx, socket, std::move(rmsg));
-  }
-
-  std::vector<std::byte> msg;
-};
-
-class AsyncTcpServiceTest : public ::testing::Test {};
-
-TEST_F(AsyncTcpServiceTest, StartTest)
-{
-  using namespace io::socket;
-
-  auto ctx = async_context{};
-  auto addr = socket_address<sockaddr_in>();
-  addr->sin_family = AF_INET;
-  auto service = async_noop_service{addr};
-
-  ctx.interrupt = [&] {
-    auto sigmask = ctx.sigmask.exchange(0);
-    for (int signum = 0; auto mask = (sigmask >> signum); ++signum)
-    {
-      if (mask & (1 << 0))
-        service.signal_handler(signum);
-    }
-  };
-
-  service.start(ctx);
-  ctx.signal(ctx.terminate);
-  while (ctx.poller.wait());
-}
-
-TEST_F(AsyncTcpServiceTest, ReadTest)
-{
-  using namespace io::socket;
-
-  auto ctx = async_context();
-  auto addr = socket_address<sockaddr_in>();
-  addr->sin_family = AF_INET;
-  addr->sin_port = htons(8080);
-  auto service = async_noop_service(addr);
-
-  ctx.interrupt = [&] {
-    auto sigmask = ctx.sigmask.exchange(0);
-    for (int signum = 0; auto mask = (sigmask >> signum); ++signum)
-    {
-      if (mask & (1 << 0))
-        service.signal_handler(signum);
-    }
-  };
-
-  service.start(ctx);
-  {
-    using namespace io;
-    auto sock = socket_handle(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    addr->sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    ASSERT_EQ(connect(sock, addr), 0);
-    ctx.poller.wait();
-
-    auto buf = std::array<char, 1>{'x'};
-    ASSERT_EQ(sendmsg(sock, socket_message{.buffers = buf}, 0), 1);
-    ctx.poller.wait();
-  }
-
-  EXPECT_EQ(static_cast<char>(service.msg.at(0)), 'x');
-  ctx.signal(ctx.terminate);
-  while (ctx.poller.wait());
-}
-
-class write_queue {
-public:
-  static constexpr auto BUFSIZE = 4096UL;
-  using buffer_type = std::array<std::byte, BUFSIZE>;
-  using queue_type = std::list<buffer_type>;
-  using iterator = queue_type::iterator;
-  using size_type = std::size_t;
-
-  auto append(std::span<const std::byte> buf) -> void
-  {
-    for (auto remaining = buf.size(), len = 0UL; remaining > 0;
-         remaining -= len)
-    {
-      len = std::min<size_type>(remaining, epptr_ - pptr_);
-      auto *begin = buf.data() + (buf.size() - remaining);
-
-      pptr_ = static_cast<std::byte *>(std::memcpy(pptr_, begin, len)) + len;
-      if (epptr_ == pptr_)
-      {
-        auto &back = queue_.emplace_back();
-        pbase_ = pptr_ = back.data();
-        epptr_ = back.data() + back.size();
-      }
-    }
-  }
-
-  auto get() -> std::span<const std::byte>
-  {
-    size_type len = 0;
-    while (!(len = egptr_ - gptr_) && egptr_ != pptr_)
-    {
-      if (egptr_ == eback_ + BUFSIZE)
-      {
-        auto &front = *queue_.erase(queue_.begin());
-        eback_ = gptr_ = front.data();
-      }
-
-      egptr_ = (eback_ == pbase_) ? pptr_ : eback_ + BUFSIZE;
-    }
-
-    return {gptr_, len};
-  }
-
-  auto size() -> size_type
-  {
-    return (queue_.size() - 1) * BUFSIZE + (pptr_ - pbase_) - (gptr_ - eback_);
-  }
-
-  auto pop(size_type n) -> void
-  {
-    for (size_type len = 0; n; n -= len)
-    {
-      len = std::min<size_type>(egptr_ - gptr_, n);
-      if ((gptr_ += len) == egptr_)
-      {
-        if (egptr_ == eback_ + BUFSIZE)
-        {
-          auto &front = *queue_.erase(queue_.begin());
-          eback_ = gptr_ = front.data();
-        }
-        egptr_ = (eback_ == pbase_) ? pptr_ : eback_ + BUFSIZE;
-      }
-    }
-  }
-
-private:
-  queue_type queue_{1};
-
-  std::byte *pbase_{queue_.back().data()}, *pptr_{queue_.back().data()},
-      *epptr_{queue_.back().data() + BUFSIZE};
-  std::byte *eback_{pptr_}, *gptr_{pptr_}, *egptr_{pptr_};
-};
-
-struct echo_service : public async_tcp_service<echo_service> {
-  using Base = async_tcp_service<echo_service>;
-
-  template <typename T>
-  explicit echo_service(socket_address<T> address) : Base(address)
-  {}
-
-  auto operator()(async_context &ctx, const socket_dialog &socket,
-                  std::shared_ptr<read_context> rmsg,
-                  std::span<const std::byte> buf) -> void
-  {
-    queue.append(buf);
-    if (queue.size() == buf.size())
-      echo(ctx, socket);
-    reader(ctx, socket, std::move(rmsg));
-  }
-
-  auto echo(async_context &ctx, const socket_dialog &socket) -> void
-  {
-    using namespace io::socket;
-    using namespace stdexec;
-    if (queue.size() == 0)
-      return;
-
-    sender auto sendmsg =
-        io::sendmsg(socket, socket_message{.buffers = {queue.get()}}, 0) |
-        then([&, socket](auto &&len) {
-          queue.pop(len);
-          echo(ctx, socket);
-        }) |
-        upon_error([](auto &&error) {});
-
-    ctx.scope.spawn(std::move(sendmsg));
-  }
-
-  write_queue queue;
-};
-
-TEST_F(AsyncTcpServiceTest, EchoTest)
-{
-  using namespace io::socket;
-
-  auto ctx = async_context();
-  auto addr = socket_address<sockaddr_in>();
-  addr->sin_family = AF_INET;
-  addr->sin_port = htons(8080);
-  auto service = echo_service(addr);
-
-  ctx.interrupt = [&] {
-    auto sigmask = ctx.sigmask.exchange(0);
-    for (int signum = 0; auto mask = (sigmask >> signum); ++signum)
-    {
-      if (mask & (1 << 0))
-        service.signal_handler(signum);
-    }
-  };
-
-  service.start(ctx);
-  {
-    using namespace io;
-    auto sock = socket_handle(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    addr->sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    ASSERT_EQ(connect(sock, addr), 0);
-    ctx.poller.wait();
-
-    auto buf = std::array<char, 1>{'x'};
-    auto msg = socket_message{.buffers = buf};
-
-    const char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-    auto *end = alphabet + 26;
-
-    for (auto *it = alphabet; it != end; ++it)
-    {
-      ASSERT_EQ(sendmsg(sock, socket_message{.buffers = std::span(it, 1)}, 0),
-                1);
-      ctx.poller.wait();
-      ASSERT_EQ(recvmsg(sock, msg, 0), 1);
-      EXPECT_EQ(buf[0], *it);
-    }
-  }
-
-  ctx.signal(ctx.terminate);
-  while (ctx.poller.wait());
-}
-
 struct echo_block_service : public async_tcp_service<echo_block_service> {
   using Base = async_tcp_service<echo_block_service>;
   using socket_message = io::socket::socket_message<>;
@@ -312,7 +72,32 @@ struct echo_block_service : public async_tcp_service<echo_block_service> {
   }
 };
 
-TEST_F(AsyncTcpServiceTest, EchoBlockTest)
+class AsyncTcpServiceTest : public ::testing::Test {};
+
+TEST_F(AsyncTcpServiceTest, StartTest)
+{
+  using namespace io::socket;
+
+  auto ctx = async_context{};
+  auto addr = socket_address<sockaddr_in>();
+  addr->sin_family = AF_INET;
+  auto service = echo_block_service{addr};
+
+  ctx.interrupt = [&] {
+    auto sigmask = ctx.sigmask.exchange(0);
+    for (int signum = 0; auto mask = (sigmask >> signum); ++signum)
+    {
+      if (mask & (1 << 0))
+        service.signal_handler(signum);
+    }
+  };
+
+  service.start(ctx);
+  ctx.signal(ctx.terminate);
+  while (ctx.poller.wait());
+}
+
+TEST_F(AsyncTcpServiceTest, EchoTest)
 {
   using namespace io::socket;
 
@@ -364,7 +149,7 @@ TEST_F(AsyncTcpServiceTest, EchoBlockTest)
   while (ctx.poller.wait());
 }
 
-TEST_F(AsyncTcpServiceTest, TestInitializeError)
+TEST_F(AsyncTcpServiceTest, InitializeError)
 {
   using namespace io::socket;
 
